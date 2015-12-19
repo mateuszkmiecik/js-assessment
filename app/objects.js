62279f49e3ecc1438acba527a0dfd3eb72ce3ed8
Mateusz Kmiecik <mk.chomik@gmail.com>
2015-12-20 00:48:02 +0100

    solution: objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+    	return Object.keys(obj).map(x => `${x}: ${obj[x]}`);
     }
   };
 });
