24af20b64c30fc358d91d1521d31fd90cbcfc587
Mateusz Kmiecik <mk.chomik@gmail.com>
2015-12-20 00:47:51 +0100

    solution: modules

--- app/modules.js
+++ app/modules.js
@@ -4,6 +4,13 @@ define(function() {
   return {
     createModule : function(str1, str2) {
 
+    	return {
+    		sayIt: function(){
+    			return `${this.greeting}, ${this.name}`
+    		},
+    		greeting: str1,
+    		name: str2
+    	}
     }
   };
 });
